34b4d198a00803e5b73b9c83587c1c8115bfd2bc
sascha <sn@sinnwerkstatt.com>
2019-08-03 18:02:44 +0200

    Slider improvements

--- wechange_payments/static/js/slider.js
+++ wechange_payments/static/js/slider.js
@@ -1,45 +1,50 @@
 $(function() {
-  
+	
+	var update_slider = function (event, ui) {
+	      
+		// set the values on slide
+		$(".payment-slider #value").val(ui.value);
+		
+		// slide the background gradient
+		var color = $('.payment-slider #slider').css('color');
+		var bg_color = $('.payment-slider #slider .background').css('background-color');
+		var max = $('.payment-slider #slider').slider("option", "max");
+		var min = $('.payment-slider #slider').slider("option", "min");
+		var percentage = (100 * (ui.value - min) / (max - min)).toFixed();
+		var style = [
+			"background: @slider-main-color",
+			"background: -moz-linear-gradient(left, " + color + " " + percentage + "%, " + bg_color + " " + percentage + "%)",
+			"background: -webkit-linear-gradient(left," + color + " " + percentage + "%, " + bg_color + " " + percentage + "%)",
+			"background: linear-gradient(to right, " + color + " " + percentage + "%, " + bg_color + " " + percentage + "%)",
+			"filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='" + color + "', endColorstr='" + bg_color + "',GradientType=1 )",
+		].join(';');
+		$('.payment-slider #slider').attr('style', style);
+		
+		// Show Different Elements based on values
+		$('.payment-slider .donation-outcome').hide();
+		if (ui.value < 10) {
+			$('.payment-slider .under-10').show();
+		} else {
+			$('.payment-slider .over-10').show();
+		}
+		
+	};
 
     $slider = $('.payment-slider #slider');
-    
     $slider.slider({
 	    value: PAYMENTS_DEFAULT_PAYMENT_AMOUNT,
 	    min: PAYMENTS_MINIMUM_PAYMENT_AMOUNT,
 	    max: PAYMENTS_MAXIMUM_PAYMENT_AMOUNT,
 	    step: 1,
-	    slide: function(event, ui){
-	      
-	      // set the values on slide
-	      $(".payment-slider #value").val(ui.value);
-	      
-	      // slide the background gradient
-	      var color = $('.payment-slider #slider').css('color');
-	      var bg_color = $('.payment-slider #slider .background').css('background-color');
-	      var max = $('.payment-slider #slider').slider("option", "max");
-	      var min = $('.payment-slider #slider').slider("option", "min");
-	      var percentage = (100 * (ui.value - min) / (max - min)).toFixed();
-	      var style = [
-	  		"background: @slider-main-color",
-			"background: -moz-linear-gradient(left, " + color + " " + percentage + "%, " + bg_color + " " + percentage + "%)",
-			"background: -webkit-linear-gradient(left,  " + color + " " + percentage + "%, " + bg_color + " " + percentage + "%)",
-			"background: linear-gradient(to right, " + color + " " + percentage + "%, " + bg_color + " " + percentage + "%)",
-			"filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='" + color + "', endColorstr='" + bg_color + "',GradientType=1 )",
-	      ].join(';');
-	      $('.payment-slider #slider').attr('style', style);
-	      
-	      // Show Different Elements based on values
-	      $('.payment-slider .donation-outcome').hide();
-	      if (ui.value < 10) {
-	        $('.payment-slider .under-10').show();
-	      } else {
-	        $('.payment-slider .over-10').show();
-	      }
-	      
-	    }
+	    slide: update_slider,
     });
     
-    // trigger slide event to update visuals
+    $('.slider-container #value').on('change keyup', function(){
+    	$slider.slider('value', this.value);
+    	$slider.slider('option', 'slide')(null, { value: this.value })
+    })
+    
+    // on initial, trigger slide event to update visuals
     $slider.slider('option', 'slide')(null, { value: $slider.slider('value') })
 });
 
