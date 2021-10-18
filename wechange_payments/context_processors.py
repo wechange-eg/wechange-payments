05e5e1dccf2b9cf839f3bbb673af85229ad7d01b
Sascha Narr <saschanarr@gmail.com>
2021-10-18 12:17:11 +0200

    Add exclusion for payments popups for members of a premium conference

--- wechange_payments/context_processors.py
+++ wechange_payments/context_processors.py
@@ -11,6 +11,7 @@ from wechange_payments.conf import settings
 from wechange_payments.models import Subscription,\
     USERPROFILE_SETTING_POPUP_CLOSED, USERPROFILE_SETTING_POPUP_CLOSED_TIMES,\
     USERPROFILE_SETTING_POPUP_USER_IS_NEW
+from cosinnus.utils.group import get_cosinnus_group_model
 
 
 logger = logging.getLogger('wechange-payments')
@@ -40,13 +41,41 @@ def current_subscription(request):
                 clicked_away_date = profile.settings.get(USERPROFILE_SETTING_POPUP_CLOSED, None)
                 threshold_date = now() - timedelta(days=settings.PAYMENTS_POPUP_SHOW_AGAIN_DAYS)
                 if clicked_away_date is None or parser.parse(clicked_away_date) < threshold_date:
-                    context.update({
-                        'show_payment_popup': True,
-                        'payment_popup_times_closed_before': profile.settings.get(USERPROFILE_SETTING_POPUP_CLOSED_TIMES, 0),
-                        'payment_popup_user_registered_after_payments': profile.settings.get(USERPROFILE_SETTING_POPUP_USER_IS_NEW, False),
-                    })
+                    do_add_popup = True
+                    
+                    # if the user would be shown a payments popup, but they are in any premium conference
+                    # that is younger than 1y, we postpone their nag-popup time by 1y after the conference ends
+                    if settings.COSINNUS_CONFERENCES_ENABLED:
+                        from cosinnus.models.conference import CosinnusConferencePremiumBlock
+                        premium_conference_delay = timedelta(weeks=52)
+                        
+                        user_group_ids = get_cosinnus_group_model().objects.get_for_user_pks(request.user)
+                        blocks_from_user_groups = CosinnusConferencePremiumBlock.objects.filter(conference__id__in=user_group_ids)
+                        block_group_ids = list(set(blocks_from_user_groups.values_list('conference_id', flat=True)))
+                        permanently_premium_groups = get_cosinnus_group_model().objects.filter(id__in=user_group_ids, is_premium_permanently=True)
+                        permanently_premium_group_ids = list(set(permanently_premium_groups.values_list('id', flat=True)))
+                        
+                        sorted_premium_groups = get_cosinnus_group_model().objects.filter(id__in=block_group_ids+permanently_premium_group_ids)\
+                                .filter(to_date__gte=now()-premium_conference_delay).order_by('-to_date')
+                        if sorted_premium_groups.count() > 0:
+                            # a premium group of less than year's end date exists, postpone for the newest
+                            do_add_popup = False
+                            postpone_date = (sorted_premium_groups[0].to_date + premium_conference_delay) - timedelta(days=settings.PAYMENTS_POPUP_SHOW_AGAIN_DAYS)
+                            # snooze popup into the future
+                            profile = request.user.cosinnus_profile
+                            profile.settings[USERPROFILE_SETTING_POPUP_CLOSED] = str(postpone_date)
+                            profile.save(update_fields=['settings'])
+                            
+                    if do_add_popup:
+                        context.update({
+                            'show_payment_popup': True,
+                            'payment_popup_times_closed_before': profile.settings.get(USERPROFILE_SETTING_POPUP_CLOSED_TIMES, 0),
+                            'payment_popup_user_registered_after_payments': profile.settings.get(USERPROFILE_SETTING_POPUP_USER_IS_NEW, False),
+                        })
                         
         except Exception as e:
             logger.error('Error in `context_processory.show_payment_popup`: %s' % e, extra={'exception': e})
+            if settings.DEBUG:
+                raise
     
     return context
