0159da1384ecf3fbcc9d28305735947cd02bebdb
Sascha Sommer <saschanarr@gmail.com>
2023-09-01 13:16:50 +0200

    Tryton: Remove time.sleep pause after invoice creation

--- wechange_payments/backends/invoice/tryton.py
+++ wechange_payments/backends/invoice/tryton.py
@@ -33,15 +33,6 @@ class TrytonInvoiceBackend(LexofficeInvoiceBackend):
     API_ENDPOINT_DOWNLOAD_INVOICE = TRYTON_API_ENDPOINT_DOWNLOAD_INVOICE
     API_ENDPOINT_CREATE_CONTACT = TRYTON_API_ENDPOINT_CREATE_CONTACT
     
-    def _create_invoice_at_provider(self, *args, **kwargs):
-        """ Tryton does not block until the PDF is created, so if we call get-document too fast after creation, it will
-            return a 404. So we wait manually.
-            TODO: remove this once tryton is blocking their request until PDF creation is finished. """
-        ret = super()._create_invoice_at_provider(*args, **kwargs)
-        import time
-        time.sleep(5)
-        return ret
-    
     def _parse_finalize_invoice_result(self, request):
         """ Helper function for `_finalize_invoice_at_provider()`, parses the resulting
             document id from the returned status 200 request or returns None if there was none.
